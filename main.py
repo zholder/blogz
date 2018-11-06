bf77d57100318896fc5be729f15cd51e04882129
Zach Holder <zacharysholder@gmail.com>
2018-11-05 20:43:07 -0600

    created and updated blogz db-- starting point for blogz

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogabuild@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:PvTKOZxheUHO95P4@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
